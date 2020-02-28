3656bc48f7c8d9ba8e32c61c6ed1405cbe3b0970
ryan m <contact@ryanmeek.com>
2020-02-28 15:51:26 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < Sinatra::Base
 
   configure() {set :public_folder, 'public'; set :views, 'app/views'}
 
+  get('/') {erb :index}
   get('/articles') {@articles = Article.all; erb :index}
   get('/articles/new') {@article = Article.new; erb :index}
 end
