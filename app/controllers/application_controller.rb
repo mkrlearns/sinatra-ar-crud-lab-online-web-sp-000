286a0237f9b5c117fd0e299a391339ec639f05b1
ryan m <contact@ryanmeek.com>
2020-02-28 16:15:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < Sinatra::Base
   get('/articles') {@articles = Article.all; erb :index}
   get('/articles/new') {@article = Article.new; erb :new}
   get('/articles/:id') {@article = Article.find(params[:id]); erb :show}
+  get('/articles/:id/edit') {@article = Article.find(params[:id]); erb :edit}
 
   post '/articles' do
     @article = Article.create(params)
